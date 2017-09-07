c5e55f0da72d35a9eaa0e4cc81aa75917a4543bb
Dariana <dari_p@msn.com>
2017-09-07 18:10:04 +0200

    Commit examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'CatShop';
 
-  myCat = {
+  myCats = [{
     'id': 1,
     'name': 'Angora',
     'description': 'Los angora son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
     'stock': 5
-  };
+  }, {
+    'id': 2,
+    'name': 'Otto',
+    'description': 'Los Ottos son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
+    'stock': 8
+  }, {
+    'id': 3,
+    'name': 'Egipcio',
+    'description': 'Los Egipcios son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
+    'stock': 0
+  }];
 }
