072203aa709ea4c58318abf6c9fa97456b0b65a6
Dariana <dari_p@msn.com>
2017-09-08 17:14:51 +0200

    Commit 17:14 08/09

--- src/app/item-list/mocks.ts
+++ src/app/item-list/mocks.ts
@@ -5,17 +5,23 @@ export const CATS: Item[] = [{
     'name': 'Angora',
     'description': 'Los angora son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
     'stock': 5,
-    'price': 12.50
+    'price': 12.50,
+    'image': '../assets/image1.jpg',
+    'selected': false
   }, {
     'id': 2,
     'name': 'Otto',
     'description': 'Los Ottos son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
     'stock': 8,
-    'price': 10.50
+    'price': 10.50,
+    'image': '../assets/image2.jpg',
+    'selected': false
   }, {
     'id': 3,
     'name': 'Egipcio',
     'description': 'Los Egipcios son una de las razas más antiguas, originaria de la región de Ankara, en Turquía centra.',
     'stock': 0,
-    'price': 15.50
+    'price': 15.50,
+    'image': '../assets/image3.jpg',
+    'selected': true
   }];
